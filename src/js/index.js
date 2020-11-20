1b8bda199fc81f38c853b2e6fe81ccf2275b0a32
xiebiao@spap.com <xiebiao@spap.com>
2020-11-20 20:53:02 +0800

    addadd
    add

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,3 @@
 import '../commons/css/reset.css'
-import "../lessAndCss/index.css";
 import "../lessAndCss/index.less";
 import '../commons/js/auto.js'
-// require("jquery");
-console.log($);
--- webpack.config.js
+++ webpack.config.js
@@ -2,8 +2,10 @@ const entry = require("./webpack.entry");
 const path = require("path");
 const plugins = require("./webpack.plugins");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+var OptimizeCssAssetsPlugin = require("optimize-css-assets-webpack-plugin"); //用于压缩css
+const devMode = process.env.NODE_ENV !== "production";
 module.exports = {
-  mode: "development",
+  mode: "production",
   entry: entry,
   output: {
     path: path.resolve(__dirname, "dist"),
@@ -20,25 +22,27 @@ module.exports = {
       {
         test: /\.less$/,
         use: [{
-          loader: MiniCssExtractPlugin.loader,
-          options: {
-            publicPath: '../'
-          }
-        }, "css-loader", "less-loader"]
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              publicPath: "../"
+            }
+          },
+          "css-loader",
+          "less-loader"
+        ]
       },
 
       {
         //使用file-loader 可以图片 嵌入 到css中
         test: /\.(png|svg|jpg|gif)$/,
         use: [{
-          loader: 'file-loader',
+          loader: "file-loader",
           options: {
-            name: 'images/[name].[ext]',
-            esModule: false, // 该项默认为true，改为false即可
+            name: "images/[name].[ext]",
+            esModule: false // 该项默认为true，改为false即可
             // outputPath: './images',
           }
-        }],
-
+        }]
       },
       {
         test: /.html$/, //所有html结尾的文件添加此 loader 处理
@@ -56,18 +60,19 @@ module.exports = {
   plugins: plugins.concat(
     new MiniCssExtractPlugin({
       filename: "./css/[name].css",
-      chunkFilename: "[id].css"
-    })
+      chunkFilename: "./css/[id].css"
+    }),
+    new OptimizeCssAssetsPlugin()
   ),
   optimization: {
     splitChunks: {
       cacheGroups: {
         //打包公共模块
         commons: {
-          chunks: 'initial', //initial表示提取入口文件的公共部分
+          chunks: "initial", //initial表示提取入口文件的公共部分
           minChunks: 2, //表示提取公共部分最少的文件数
           minSize: 0, //表示提取公共部分最小的大小
-          name: 'commons' //提取出来的文件命名
+          name: "commons" //提取出来的文件命名
         }
       }
     }
